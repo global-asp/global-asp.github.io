3857665f05829c7262ef6af0f3fab512107d430b
dohliam <dohliam@gmail.com>
2015-10-16 02:13:39 -0700

    thanks (fix)

--- experiments/gt-no/translator.js
+++ experiments/gt-no/translator.js
@@ -126,7 +126,7 @@ function review_translation() {
   var review_table = document.getElementById("review_table");
   review_table.innerHTML = content_div + "<tr><td></td><td>" + format_attribution + "</td></tr></table>";
 
-  document.getElementById("thanks").value = "/translator/thanks.html?" + idx;
+  document.getElementById("thanks").value = "thanks.html?" + idx;
 
   overlay('final');
 
